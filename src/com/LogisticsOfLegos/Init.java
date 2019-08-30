71e20a20eb629b85e46e560f5c12f4dfdf372e13
Mike Sütö <i18040@hb.dhbw-stuttgart.de>
2019-08-30 21:05:01 +0200

    Init.java hinzugefügt, damit man ne main class hat, die alles startet

---
+++ src/com/LogisticsOfLegos/Init.java
@@ -0,0 +1,9 @@
+package com.LogisticsOfLegos;
+
+import com.LogisticsOfLegos.Movement.commands;
+
+public class Init {
+  public static void main(String[] args){
+    new commands().initialize();
+  }
+}
